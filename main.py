982db31687c7d751816b0f0ab0a9f41189f0acba
Thiago Felps <thiagofelps@MacBook-Air-de-Thiago.local>
2025-07-05 11:14:53 -0300

    feat: rota / adicionada

--- main.py
+++ main.py
@@ -62,3 +62,15 @@ if __name__ == "__main__":
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "MineraZap está online!"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
